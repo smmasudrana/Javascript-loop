cb57cf9ec7d04bfa0f3c56fe3a024e1f7a3a0f7e
smmasudrana <smmasudrana62@gmail.com>
2025-07-30 12:41:50 +0600

    add while loop

--- javascript.js
+++ javascript.js
@@ -13,3 +13,28 @@ for(const fruit of fruits) {
     console.log(fruit);
     console.log('khabo');
 }
+
+
+const number =7;
+if (number < 10){
+    console.log('soto akta number paisi')
+}
+console.log('something is cooking')
+
+
+// while loop
+let num =0;
+while(num < 5) {
+    console.log('loop hotey thak');
+    num = num +1;
+    // num +=1; shortcut
+    // num ++;
+}
+
+let num =1;
+while(num <=10){
+    console.log('kire valo achis?');
+    num++;
+}
+
+
---
+++ sum.js
@@ -0,0 +1,19 @@
+let num =1;
+let sum = 0; 
+while (num <= 10){
+    console.log(num);
+    sum = sum+num;
+    console.log('sum:', sum)
+    num++;
+}
+
+
+let num = 1;
+let masud= 0;
+while(num <= 20) {
+    console.log(num);
+    masud = masud+num;
+    console.log('masud:', masud)
+    num++;
+}
+
